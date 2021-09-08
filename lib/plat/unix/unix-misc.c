9933b1a0838918f055719707807211e76899a735
weifeng <weifeng.huang@tuya.com>
2021-09-08 11:35:43 +0800

    rewrite the clock_gettime, disable -mmacosx-version-min=10.15

--- lib/plat/unix/unix-misc.c
+++ lib/plat/unix/unix-misc.c
@@ -27,6 +27,11 @@
 #endif
 #include "private-lib-core.h"
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 /*
  * Normally you don't want this, use lws_sul instead inside the event loop.
  * But sometimes for drivers it makes sense, so there's an internal-only
@@ -45,9 +50,18 @@ lws_now_usecs(void)
 #if defined(LWS_HAVE_CLOCK_GETTIME)
 	struct timespec ts;
 
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+        clock_serv_t cclock;
+        mach_timespec_t mts;
+        host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+        clock_get_time(cclock, &mts);
+        mach_port_deallocate(mach_task_self(), cclock);
+        ts.tv_sec = mts.tv_sec;
+        ts.tv_nsec = mts.tv_nsec;
+#else 
 	if (clock_gettime(CLOCK_MONOTONIC, &ts))
 		return 0;
-
+#endif
 	return (((lws_usec_t)ts.tv_sec) * LWS_US_PER_SEC) +
 			((lws_usec_t)ts.tv_nsec / LWS_NS_PER_US);
 #else
